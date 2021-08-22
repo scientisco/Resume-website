7cf7665389dd19c268d11643deffac02eec4c1de
Adeyinka Anees <71608014+scientisco@users.noreply.github.com>
2021-08-22 08:26:24 +0100

    Js added

---
+++ script.js
@@ -0,0 +1,35 @@
+// window.addEventListener("scroll",()=>
+// {   
+//     var x = window.matchMedia("(max-width: 540px)")
+//     let contents = document.querySelectorAll(".anim");
+//     contents.forEach(content => {
+    
+//     let contentPosition = content.getBoundingClientRect().top;
+
+//     let screenPosition = window.innerHeight/1.2;
+    
+//     if (contentPosition < screenPosition){
+//         content.classList.add("active");
+        
+//     }else{
+//         content.classList.remove("active");
+        
+//     }
+        
+//     });
+//     let contents2 = document.querySelectorAll(".anim2");
+//     contents2.forEach(content => {
+    
+//     let contentPosition = content.getBoundingClientRect().top;
+//     let screenPosition = window.innerHeight/1.5 ;
+//     if (contentPosition < screenPosition){
+//         content.classList.add("active2");
+        
+//     }else{
+//         content.classList.remove("active2");
+        
+//     }
+        
+//     });
+    
+// });
