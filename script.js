0ae34e16f7042f3171a999248858ae4b349f8a55
Scientisco <adeyinkaanees@yahoo.com>
2022-01-21 01:05:23 +0100

    Updated resume

---
+++ script.js
@@ -0,0 +1,50 @@
+// window.addEventListener("scroll",()=>
+// {   
+//     var x = window.matchMedia("(max-width: 540px)")
+//     let contents = document.querySelectorAll(".anim");
+//     contents.forEach(content => {
+    
+//     let contentPosition = content.getBoundingClientRect().top;
+
+//     let screenPosition = window.innerHeight/1.2;
+    
+//     if (contentPosition < screenPosition){
+//         content.classList.add("active");
+        
+//     }else{
+//         content.classList.remove("active");
+        
+//     }
+        
+//     });
+//     let contents2 = document.querySelectorAll(".anim2");
+//     contents2.forEach(content => {
+    
+//     let contentPosition = content.getBoundingClientRect().top;
+//     let screenPosition = window.innerHeight/1.5 ;
+//     if (contentPosition < screenPosition){
+//         content.classList.add("active2");
+        
+//     }else{
+//         content.classList.remove("active2");
+        
+//     }
+        
+//     });
+    
+// });
+
+// function devicetype(width){
+//     if (width.matches){
+//         document.body.style.backgroundColor = "red";
+        
+//         alert(y);
+//         alert("it is ");
+//     }else{
+//         document.body.style.backgroundColor = "blue";
+//     }
+// }
+
+// var width = window.matchMedia("(max-width: 500px)");
+// devicetype(width);
+// width.addEventListener("click",devicetype);
